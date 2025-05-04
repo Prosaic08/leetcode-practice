22e85afe699510f8d75b8ed3c086d5d6ee33f263
Sujay Prasad <prasadsujay0@gmail.com>
2025-05-04 22:00:46 +0100

    New Folder

---
+++ arrays/two-sum.py.py
@@ -0,0 +1,14 @@
+"""My first thought process was to loop through
+each index of the list. I realized that this method would be ineffient since its O(n^2)"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums):
+                if nums[j] + nums[i] == target:
+                    answer = [i, j]
+                j += 1
+            i += 1
+        return answer
