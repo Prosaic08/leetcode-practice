68cab37dca2478ab287d649629ba758d7877062a
Sujay Prasad <prasadsujay0@gmail.com>
2025-05-08 00:15:30 +0100

    Updated contains-duplicate.py with O(n)

--- arrays/contains-duplicate.py
+++ arrays/contains-duplicate.py
@@ -1,14 +1,5 @@
-"""made a duplicate list which is empty. I looped through nums. For every index,
-I would append it to the duplicate list that Ive seen so far.
-If value was already in duplicate, then would return True.
-If loop finishes without finding any duplicates, return False"""
+"""I compared the length of the set of nums to the length of actual nums list"""
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        duplicate = []
-        for i in nums:
-            if i in duplicate:
-                return True
-            else:
-                duplicate.append(i)
-        return False
+        return len(set(nums)) != len(nums)
