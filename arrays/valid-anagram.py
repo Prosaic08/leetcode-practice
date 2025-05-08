1da98270f1ef77ec77f45ea11854f043cc8cd173
Sujay Prasad <prasadsujay0@gmail.com>
2025-05-08 20:29:27 +0100

    Updated valid-anagram.py with O(n) hashmap

--- arrays/valid-anagram.py
+++ arrays/valid-anagram.py
@@ -1,12 +1,16 @@
-"""First I compared the length of the two words, if not, they arent anagrams. Then
-I went through each character in the first word. If count of that character is not the 
-same in both words, its not an anagram, if they match, they are anagrams."""
+"""First, we compare the lengths of the two strings. If they are not the same, we return false. 
+We then create two hashmaps to count the letters in each string. If the two dictionaries are the same, 
+we return true; otherwise, we return false. In time complexity this solution is O(n+m)
+however solution may take too  much memory space depending on the length of the input strings."""
+
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
+            if len(s) != len(t):
+                return False
+            sMap, tMap = {}, {}
             for i in s:
-                if s.count(i) != t.count(i):
-                    return False
-            return True
-        return False
+                sMap[i] = sMap.get(i, 0) + 1
+            for i in t:
+                tMap[i] = tMap.get(i, 0) + 1
+            return sMap == tMap
